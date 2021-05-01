d85dc0e7846acda100bb802ac52ffae3796dee6d
SherryH <enjoy.wind7rain@gmail.com>
2021-05-01 11:06:15 +0200

    Added User access control

--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -1,9 +1,19 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
+import { permissions, rules } from '../access';
 
 export const User = list({
-  access: {},
-  ui: {},
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    // hide the backend UI from regular users
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({
@@ -24,6 +34,10 @@ export const User = list({
     role: relationship({
       ref: 'Role.assignedTo',
       // TODO: Add Access Control
+      access: {
+        update: permissions.canManageUsers,
+        delete: permissions.canManageUsers,
+      },
     }),
     products: relationship({
       ref: 'Product.user',
